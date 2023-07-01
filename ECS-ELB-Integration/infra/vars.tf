ddee31b07be2d6cf886f207a58fb78f6958d305c
Durvesh-palkar <palkardurvesh16032000@gmail.com>
2023-07-01 23:25:03 +0530

    adding nat gw for private subnet

--- ECS-ELB-Integration/infra/vars.tf
+++ ECS-ELB-Integration/infra/vars.tf
@@ -19,7 +19,7 @@ variable "cidr" {
 
 variable "private_subnets" {
   description = "a list of CIDRs for private subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
-  default     = ["10.0.0.0/20", "10.0.32.0/20"]
+  default     = ["10.0.0.0/20"]
 }
 
 variable "public_subnets" {
